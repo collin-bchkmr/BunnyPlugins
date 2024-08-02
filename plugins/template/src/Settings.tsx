b618d247fcb879f0dbedfe77f3e4962d825a9895
Collin <collin@lona-development.org>
2024-08-02 21:04:39 +0000

    weeeeeeeeeee

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -1,10 +1,40 @@
-import { Forms } from "@vendetta/ui/components";
-const { FormText } = Forms;
-
-export default () => {
+import {
+    clipboard,
+    NavigationNative,
+    React,
+    ReactNative as RN,
+  } from "@vendetta/metro/common";
+  import { useProxy } from "@vendetta/storage";
+  import { showConfirmationAlert } from "@vendetta/ui/alerts";
+  import { getAssetIDByName } from "@vendetta/ui/assets";
+  import { Forms } from "@vendetta/ui/components";
+  import { showToast } from "@vendetta/ui/toasts";
+  import { BetterTableRowGroup } from "./BetterTableRow";
+  
+  const { FormSwitchRow, FormIcon, FormRow } = Forms;
+  
+  export default () => {
     return (
-        <FormText>
-            Hello, world!
-        </FormText>
-    )   
-}
+      <>
+        <RN.ScrollView
+          style={{
+            zIndex: 1,
+          }}
+        >
+          <BetterTableRowGroup
+            title="TEEEEEEEEEEEE"
+          >
+          </BetterTableRowGroup>
+          <BetterTableRowGroup
+            title="Settings"
+          >
+            <FormSwitchRow
+              label="WEEWEE Mode"
+              subLabel="UWU"
+              value={"vstorage.settings.edit"}
+            />
+          </BetterTableRowGroup>
+        </RN.ScrollView>
+      </>
+    );
+  };

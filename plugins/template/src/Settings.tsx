1cfad524cb2315e653c676c48405e877923fbf94
Collin <collin@lona-development.org>
2024-08-02 12:16:15 +0000

    test

--- plugins/template/src/Settings.tsx
+++ plugins/template/src/Settings.tsx
@@ -1,27 +1,8 @@
 import { Forms } from "@vendetta/ui/components";
-import { patchSettingsPin } from "$/lib/pinToSettings";
-
 const { FormText } = Forms;
 
-export default (): (() => void) => {
-    const patches = [];
-    patches.push(
-        patchSettingsPin(
-            () => true, // Assuming the section should always be shown
-            () => <SettingsSection changes="Bunny" />, // Pass "Bunny" for changes
-            {
-                key: plugin.manifest.name,
-                icon: getAssetIDByName(manifest.vendetta.icon),
-                trailing: () => {
-                    // ... existing trailing logic
-                },
-                title: () => lang.format("plugin.name", {}),
-                page: {
-                    render: PluginBrowserPage,
-                },
-            },
-        ),
-    );
-
-    return () => patches.forEach((x) => x());
-};
+export default () => (
+    <FormText>
+        Hello, world!
+    </FormText>
+)
--- plugins/template/src/index.ts
+++ plugins/template/src/index.ts
@@ -1,9 +1,25 @@
 import { logger } from "@vendetta";
 import Settings from "./Settings";
+import { getAssetIDByName } from "@lib/api/assets";
+import { registerSection } from "@lib/ui/settings";
 
 export default {
     onLoad: () => {
         logger.log("Hello world!");
+        registerSection({
+            name: "Hymmel",
+            items: [
+                {
+                    key: "HYMMEL",
+                    title: () => "Hymmel",
+                    icon: getAssetIDByName("WrenchIcon"),
+                    render: () => import("@core/ui/settings/pages/General"),
+                    rawTabsConfig: {
+                        useTrailing: () => "ABC TEST"
+                    }
+                }
+            ]
+        });
     },
     onUnload: () => {
         logger.log("Goodbye, world.");

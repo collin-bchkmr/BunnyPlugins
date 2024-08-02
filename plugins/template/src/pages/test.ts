ed6775cec1aedd62428d00d7c16c774a0f860eae
Collin <collin@lona-development.org>
2024-08-02 13:13:43 +0000

    wooooo

---
+++ plugins/template/src/pages/test.ts
@@ -0,0 +1,17 @@
+import { Stack, TableRow, TableRowGroup } from "@vendetta/ui/components";
+import { ScrollView } from "react-native";
+
+export default function General() {
+    return (
+        <ScrollView style={{ flex: 1 }} contentContainerStyle={{ paddingBottom: 38 }}>
+            <Stack style={{ paddingVertical: 24, paddingHorizontal: 12 }} spacing={24}>
+                <TableRowGroup title={"ABC"}>
+                    <TableRow
+                        label={"TEST"}
+                        trailing={<TableRow.TrailingText text={"BHABJ"} />}
+                    />
+                </TableRowGroup>
+            </Stack>
+        </ScrollView>
+    );
+}

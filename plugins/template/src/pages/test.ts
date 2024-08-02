e74d7e116bbdcbce4551cf52d63484556c934e5e
Collin <collin@lona-development.org>
2024-08-02 13:18:44 +0000

    i cry

--- plugins/template/src/pages/test.ts
+++ plugins/template/src/pages/test.ts
@@ -3,8 +3,8 @@ import { ScrollView } from "react-native";
 
 export default function test() {
     return (
-        <ScrollView style={{ flex: 1 }} contentContainerStyle={{ paddingBottom: 38 }}>
-            <Stack style={{ paddingVertical: 24, paddingHorizontal: 12 }} spacing={24}>
+        <ScrollView>
+            <Stack>
                 <TableRowGroup title={"ABC"}>
                     <TableRow
                         label={"TEST"}

4d61c6d4c65db80dcb20e82f076e7a371c1f9c10
Collin <collin@lona-development.org>
2024-08-02 13:26:52 +0000

    w

--- plugins/template/src/pages/test.ts
+++ plugins/template/src/pages/test.ts
@@ -1,17 +1,10 @@
-import { Stack, TableRow, TableRowGroup } from "@vendetta/ui/components/discord/Redesign";
-import { ScrollView } from "react-native";
+import { Forms } from "@vendetta/ui/components";
+const { FormText } = Forms;
 
-export default function test() {
+export default () => {
     return (
-        <ScrollView>
-            <Stack>
-                <TableRowGroup title={"ABC"}>
-                    <TableRow
-                        label={"TEST"}
-                        trailing={<TableRow.TrailingText text={"BHABJ"} />}
-                    />
-                </TableRowGroup>
-            </Stack>
-        </ScrollView>
-    );
+        <FormText>
+            Hello, world!
+        </FormText>
+    )   
 }
